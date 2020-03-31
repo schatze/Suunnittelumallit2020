5214fbca0ddc3dba786cff19ac26863042427566
schatze <oskar@schatz.fi>
2020-03-31 10:02:38 +0300

    Tehtävä 2 bonus

--- 02 AbstractFactory/src/abstractfactory/Main.java
+++ 02 AbstractFactory/src/abstractfactory/Main.java
@@ -9,6 +9,9 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 import abstractfactory.Vaatteet.Vaate;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 /**
  *
@@ -20,8 +23,21 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Properties properties = new Properties();
+        
+        try {
+            properties.load(new FileInputStream("src/abstractfactory/tehdas.properties"));
+        } catch (IOException exception) {
+            System.out.println("Error reading properties");
+            exception.printStackTrace();
+            return;
+        }
+        
+        String tehdas = properties.getProperty("tehdas").toString();
+        System.out.println("Valittu tehdas on " + tehdas);
+        
         Set<Vaate> vaatteet = new HashSet();
-        AbstractFactory vaateTehdas = TehtaanTuottaja.haeTehdas("adidas");  // tässä voi vaihtaa merkkiä/tehdasta
+        AbstractFactory vaateTehdas = TehtaanTuottaja.haeTehdas(tehdas);  // tässä voi vaihtaa merkkiä/tehdasta
         
         vaatteet.add(vaateTehdas.haeVaate("paita"));
         vaatteet.add(vaateTehdas.haeVaate("päähine"));
